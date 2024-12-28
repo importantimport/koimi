6d780a4c89e5558a023d77657ff3fe456f7e48cd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-29 00:05:14 +0800

    refactor(examples/basics): import from astro

--- examples/basics/src/content.config.ts
+++ examples/basics/src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from 'astro/loaders'
-import { defineCollection, z } from 'astro:content'
+import { z } from 'astro/zod'
+import { defineCollection } from 'astro:content'
 
 const articles = defineCollection({
   loader: glob({ base: './src/contents/articles', pattern: '**/*.{md,mdx}' }),

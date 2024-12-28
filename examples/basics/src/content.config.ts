d64edb94e618371c65fe562b342405cf3d303276
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-28 22:26:55 +0800

    chore: update content config

--- examples/basics/src/content.config.ts
+++ examples/basics/src/content.config.ts
@@ -3,7 +3,14 @@ import { defineCollection, z } from 'astro:content'
 
 const articles = defineCollection({
   loader: glob({ base: './src/contents/articles', pattern: '**/*.{md,mdx}' }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
+    image: z.union([
+      image(),
+      z.object({
+        alt: z.string(),
+        src: image(),
+      }),
+    ]).optional(),
     // permalink: z.string().optional(),
     title: z.string(),
   }),
